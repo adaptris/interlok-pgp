7574a21d20f0063c6aac26f50464c10ca9a7912d
Ashley Morgan Anderson <amanderson@albinoloverats.net>
2019-10-07 14:35:14 +0100

    Add helper methods

--- src/main/java/com/adaptris/security/pgp/Utils.java
+++ src/main/java/com/adaptris/security/pgp/Utils.java
@@ -1,12 +1,18 @@
 package com.adaptris.security.pgp;
 
 import org.bouncycastle.bcpg.ArmoredInputStream;
+import org.bouncycastle.openpgp.PGPSignature;
+import org.bouncycastle.openpgp.PGPSignatureGenerator;
+import org.bouncycastle.util.Strings;
 import org.bouncycastle.util.encoders.Base64;
 import org.bouncycastle.util.encoders.DecoderException;
 
 import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.security.SignatureException;
 
 class Utils
 {
@@ -116,5 +122,120 @@ class Utils
 			}
 			return result;
 		}
+	}
+
+	static void processLine(OutputStream aOut, PGPSignatureGenerator sGen, byte[] line) throws SignatureException, IOException
+	{
+		// note: trailing white space needs to be removed from the end of
+		// each line for signature calculation RFC 4880 Section 7.1
+		int length = Utils.getLengthWithoutWhiteSpace(line);
+		if (length > 0)
+		{
+			sGen.update(line, 0, length);
+		}
+		aOut.write(line, 0, line.length);
+	}
+
+	static void processLine(PGPSignature sig, byte[] line) throws SignatureException, IOException
+	{
+		int length = Utils.getLengthWithoutWhiteSpace(line);
+		if (length > 0)
+		{
+			sig.update(line, 0, length);
+		}
+	}
+
+	static int getLengthWithoutWhiteSpace(byte[] line)
+	{
+		int    end = line.length - 1;
+		while (end >= 0 && isWhiteSpace(line[end]))
+		{
+			end--;
+		}
+		return end + 1;
+	}
+
+	static int getLengthWithoutSeparatorOrTrailingWhitespace(byte[] line)
+	{
+		int    end = line.length - 1;
+
+		while (end >= 0 && isWhiteSpace(line[end]))
+		{
+			end--;
+		}
+
+		return end + 1;
+	}
+
+	static byte[] getLineSeparator()
+	{
+		String nl = Strings.lineSeparator();
+		byte[] nlBytes = new byte[nl.length()];
+
+		for (int i = 0; i != nlBytes.length; i++)
+		{
+			nlBytes[i] = (byte)nl.charAt(i);
+		}
+
+		return nlBytes;
+	}
+
+	private static boolean isWhiteSpace(byte b)
+	{
+		return isLineEnding(b) || b == '\t' || b == ' ';
+	}
+
+	private static boolean isLineEnding(byte b)
+	{
+		return b == '\r' || b == '\n';
+	}
+
+	static int readInputLine(ByteArrayOutputStream bOut, InputStream fIn) throws IOException
+	{
+		bOut.reset();
+		int lookAhead = -1;
+		int ch;
+		while ((ch = fIn.read()) >= 0)
+		{
+			bOut.write(ch);
+			if (ch == '\r' || ch == '\n')
+			{
+				lookAhead = readPassedEOL(bOut, ch, fIn);
+				break;
+			}
+		}
+		return lookAhead;
+	}
+
+	static int readInputLine(ByteArrayOutputStream bOut, int lookAhead, InputStream fIn) throws IOException
+	{
+		bOut.reset();
+		int ch = lookAhead;
+		do
+		{
+			bOut.write(ch);
+			if (ch == '\r' || ch == '\n')
+			{
+				lookAhead = readPassedEOL(bOut, ch, fIn);
+				break;
+			}
+		}
+		while ((ch = fIn.read()) >= 0);
+		if (ch < 0)
+		{
+			lookAhead = -1;
+		}
+		return lookAhead;
+	}
+
+	private static int readPassedEOL(ByteArrayOutputStream bOut, int lastCh, InputStream fIn) throws IOException
+	{
+		int lookAhead = fIn.read();
+		if (lastCh == '\r' && lookAhead == '\n')
+		{
+			bOut.write(lookAhead);
+			lookAhead = fIn.read();
+		}
+		return lookAhead;
 	}
 }
